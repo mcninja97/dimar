91c1d72dcf76e46d4901f56b8ab025f5f2a89abd
Ryan McDonnell <97rmcdonnell@gmail.com>
2020-12-17 22:57:47 -0500

    increase refresh time and fix typo

--- dimar.js
+++ dimar.js
@@ -1,5 +1,6 @@
-setInterval(click_refresh, 150000);
+setInterval(click_refresh, 300000);
 
 function click_refresh(){
 	document.querySelector('[title="Refresh Destiny Data [R]"]').click();
+	console.log("refreshed by dimar");
 }
